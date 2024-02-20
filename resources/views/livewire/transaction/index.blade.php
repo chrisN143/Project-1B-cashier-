91da73477119944e5b52c8540fa142b824b4ddd6
roysuryo <reshasalim55@gmail.com>
2024-02-20 10:30:46 +0700

    pull product

--- resources/views/livewire/transaction/index.blade.php
+++ resources/views/livewire/transaction/index.blade.php
@@ -5,11 +5,8 @@
                 <tr class="text-start text-muted fw-bold fs-7 text-uppercase gs-0">
                     <th class="min-w-25px sorting" style="width: 25px;">No</th>
                     <th class="min-w-125px sorting" style="width: 125px;">Nomor</th>
-                    <th class="min-w-125px sorting" style="width: 125px;">
-                        Nama</th>
-    
-                    <th class="min-w-125px sorting" style="width: 125px;">
-                        Action</th>
+                    <th class="min-w-125px sorting" style="width: 125px;">Nama</th>
+                    <th class="min-w-125px sorting" style="width: 125px;">Action</th>
                 </tr>
             </thead>
             <tbody class="text-gray-600 fw-semibold">
@@ -17,14 +14,12 @@
                     <tr>
                         <td>{{ $index + 1 }}</td>
                         <td>{{ $item['payment_method'] }}</td>
-                
                         <td>
                             <a class='btn btn-primary' href="{{ route('transaction.detail', ['id' => $item['id']]) }}">
                                 Edit
                             </a>
                             <button class='btn btn-danger' wire:click='destroy({{ $item['id'] }})' 
-                                wire:confirm="Are you sure you want to delete this post?"
->
+                                wire:confirm="Are you sure you want to delete this post?">
                                 Hapus
                             </button>
                         </td>
