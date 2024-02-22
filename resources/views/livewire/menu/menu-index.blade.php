12c5a86c7fc47608d33b41cc9ec88967ab72929f
Mr43344 <micheladlya@gmail.com>
2024-02-22 16:19:33 +0700

    anjayyyy

--- resources/views/livewire/menu/menu-index.blade.php
+++ resources/views/livewire/menu/menu-index.blade.php
@@ -53,7 +53,7 @@
                             <div class="d-flex mx-3">
                                 @if ($this->inputquantity >= 0)
                                     <input type="number" wire:model.live="inputquantity"
-                                        value="{{ $this->inputquantity }}" class="form-control me-3"
+                                        value="{{ $this->inputquantity }}" min="1" class="form-control me-3"
                                         style="width:60px;">
                                     <button wire:click="add({{ $item->id }})"
                                         class="btn btn-info btn-sm text-white">
