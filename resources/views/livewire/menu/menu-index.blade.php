13ead73130d3f81e379d0ed045d8e05dd80d1ec2
chrisN143 <ganichristiann@gmail.com>
2024-02-27 13:05:56 +0700

    test

--- resources/views/livewire/menu/menu-index.blade.php
+++ resources/views/livewire/menu/menu-index.blade.php
@@ -2,6 +2,8 @@
     @if (session('status'))
         <div class="alert alert-success" role="alert">
             {{ session('status') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+
         </div>
     @endif
     @if (session('error'))
