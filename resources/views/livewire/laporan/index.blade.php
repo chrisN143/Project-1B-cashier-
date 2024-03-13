ee49c0176161a83854cb9f327a82d0cbdad28a76
Skyisblue324 <skyisblue324568@gmail.com>
2024-03-13 14:20:50 +0700

    Done

--- resources/views/livewire/laporan/index.blade.php
+++ resources/views/livewire/laporan/index.blade.php
@@ -1,61 +1,7 @@
 <div>
-    <div class="row g-5 gx-xl-10 mb-5 mb-xl-10 ">
-        <!--begin::Col-->
-        <div class="col-md-4">
-            <!--begin::Card widget 7-->
-            <div class="card card-flush bg-primary bg-gradient">
-                <!--begin::Header-->
-                <div class="card-header py-4">
-                    <!--begin::Title-->
-                    <div class="card-title d-flex flex-column ">
-                        <!--begin::Amount-->
-                        <span class="text-light fw-semibold fs-2">All orders</span>
-                        <span class="fs-2 text-light lh-1 ls-n2">{{ $ordersCount }}</span>
-                        <span class="text-light fw-semibold fs-6">Created At</span>
-                        <!--end::Amount-->
-
-                        <!--begin::Subtitle-->
-                        <!--end::Subtitle-->
-                    </div>
-                    <span class="float-right display-5 bg-opacity-25"><i
-                            class="fa-solid fa-list fs-1 text-light"></i></span>
-                    <!--end::Title-->
-                </div>
-                <!--end::Header-->
-            </div>
-            <!--end::Card widget 7-->
-        </div>
-        <!--end::Col-->
-        <!--begin::Col-->
-        <div class="col-md-4">
-            <!--begin::Card widget 7-->
-            <div class="card card-flush bg-danger bg-gradient">
-                <!--begin::Header-->
-                <div class="card-header py-4">
-                    <!--begin::Title-->
-                    <div class="card-title d-flex flex-column">
-                        <!--begin::Amount-->
-                        <span class="text-light  fw-semibold fs-2">Incomme</span>
-                        <h4 class="fs-2 text-light lh-1 ls-n2">Rp. {{ number_format($ordersPrice, 0, ',', '.') }}</h4>
-
-                        <span class="text-light fw-semibold fs-6">Created At</span>
-                        <!--end::Amount-->
-
-                        <!--begin::Subtitle-->
-                        <!--end::Subtitle-->
-                    </div>
-                    <span class="float-right display-5 bg-opacity-25"><i
-                            class="fa-solid fa-dollar-sign fs-1 text-light"></i></span>
-                    <!--end::Title-->
-                </div>
-                <!--end::Header-->
-            </div>
-            <!--end::Card widget 7-->
-        </div>
-    </div>
+    @livewire('laporan.index-income')
     <div class="tab-content" id="myTabContent">
-        <div class="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab"
-            tabindex="0">
+        <div class="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
             <div class="row py-3">
                 <div class="col-md-2 my-2">
                     <input id="start" class="form-control" type="date" wire:model.live='start_date'>
