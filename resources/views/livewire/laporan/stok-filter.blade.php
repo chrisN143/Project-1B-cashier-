d66e8b3070f995420aeb451c48e01271dc2b2321
RickyWijaya0 <rickyweb1107@gmail.com>
2024-03-14 15:15:28 +0700

    aa

--- resources/views/livewire/laporan/stok-filter.blade.php
+++ resources/views/livewire/laporan/stok-filter.blade.php
@@ -4,7 +4,7 @@
             @foreach ($itemCounts as $itemCount)
                 <tr>
                     <td>{{ $itemCount['product_name'] }}</td>
-                    <td>{{ $itemCount['total_quantity'] }}</td>
+                    <td>{{ $itemCount['total_quantity'] }}</td>x    
                 </tr>
             @endforeach
         </tbody>
